98bd38f508fbc1dbd62342ab1028b1222ebcb82d
Dmitry Ilvokhin <d@ilvokhin.com>
2026-04-29 12:02:08 +0000

    mm: use zone lock guard in unreserve_highatomic_pageblock()
    Use spinlock_irqsave zone lock guard in unreserve_highatomic_pageblock()
    to replace the explicit lock/unlock pattern with automatic scope-based
    cleanup.
    
    Link: https://lore.kernel.org/69db814cd178915cb5615334a29304678f960963.1777462630.git.d@ilvokhin.com
    Signed-off-by: Dmitry Ilvokhin <d@ilvokhin.com>
    Suggested-by: Steven Rostedt <rostedt@goodmis.org>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Cc: Brendan Jackman <jackmanb@google.com>
    Cc: David Hildenbrand <david@kernel.org>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Lorenzo Stoakes <ljs@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Vlastimil Babka <vbabka@kernel.org>
    Cc: Zi Yan <ziy@nvidia.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -3481,7 +3481,6 @@ static bool unreserve_highatomic_pageblock(const struct alloc_context *ac,
 						bool force)
 {
 	struct zonelist *zonelist = ac->zonelist;
-	unsigned long flags;
 	struct zoneref *z;
 	struct zone *zone;
 	struct page *page;
@@ -3498,7 +3497,7 @@ static bool unreserve_highatomic_pageblock(const struct alloc_context *ac,
 					pageblock_nr_pages)
 			continue;
 
-		spin_lock_irqsave(&zone->lock, flags);
+		guard(spinlock_irqsave)(&zone->lock);
 		for (order = 0; order < NR_PAGE_ORDERS; order++) {
 			struct free_area *area = &(zone->free_area[order]);
 			unsigned long size;
@@ -3545,12 +3544,9 @@ static bool unreserve_highatomic_pageblock(const struct alloc_context *ac,
 			 * so this should not fail on zone boundaries.
 			 */
 			WARN_ON_ONCE(ret == -1);
-			if (ret > 0) {
-				spin_unlock_irqrestore(&zone->lock, flags);
+			if (ret > 0)
 				return ret;
-			}
 		}
-		spin_unlock_irqrestore(&zone->lock, flags);
 	}
 
 	return false;
