1f4d487e08d862b762cfcf5133ecacac355d897c
Ravi Jonnalagadda <ravis.opensrc@gmail.com>
2026-05-11 22:04:35 -0700

    mm/damon/core: add damon_unregister_ops for clean module removal
    Add damon_unregister_ops() which zeroes the registered ops entry,
    preventing use-after-free when IBS/PEBS modules are unloaded.
    Also export damon_register_ops() for module use.
    
    Signed-off-by: Ravi Jonnalagadda <ravis.opensrc@gmail.com>

--- include/linux/damon.h
+++ include/linux/damon.h
@@ -1204,6 +1204,7 @@ int damon_commit_ctx(struct damon_ctx *old_ctx, struct damon_ctx *new_ctx);
 int damon_nr_running_ctxs(void);
 bool damon_is_registered_ops(enum damon_ops_id id);
 int damon_register_ops(struct damon_operations *ops);
+int damon_unregister_ops(enum damon_ops_id id);
 int damon_select_ops(struct damon_ctx *ctx, enum damon_ops_id id);
 
 static inline bool damon_target_has_pid(const struct damon_ctx *ctx)
