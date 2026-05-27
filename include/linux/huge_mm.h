80cff0105aa71c00612e85ffc58ee22001d1802c
Johannes Weiner <hannes@cmpxchg.org>
2026-05-27 16:45:16 -0400

    mm: switch deferred split shrinker to list_lru
    The deferred split queue handles cgroups in a suboptimal fashion.  The
    queue is per-NUMA node or per-cgroup, not the intersection.  That means on
    a cgrouped system, a node-restricted allocation entering reclaim can end
    up splitting large pages on other nodes:
    
            alloc/unmap
              deferred_split_folio()
                list_add_tail(memcg->split_queue)
                set_shrinker_bit(memcg, node, deferred_shrinker_id)
    
            for_each_zone_zonelist_nodemask(restricted_nodes)
              mem_cgroup_iter()
                shrink_slab(node, memcg)
                  shrink_slab_memcg(node, memcg)
                    if test_shrinker_bit(memcg, node, deferred_shrinker_id)
                      deferred_split_scan()
                        walks memcg->split_queue
    
    The shrinker bit adds an imperfect guard rail.  As soon as the cgroup has
    a single large page on the node of interest, all large pages owned by that
    memcg, including those on other nodes, will be split.
    
    list_lru properly sets up per-node, per-cgroup lists.  As a bonus, it
    streamlines a lot of the list operations and reclaim walks.  It's used
    widely by other major shrinkers already.  Convert the deferred split queue
    as well.
    
    The list_lru per-memcg heads are instantiated on demand when the first
    object of interest is allocated for a cgroup, by calling
    folio_memcg_alloc_deferred().  Add calls to where splittable pages are
    created: anon faults, swapin faults, khugepaged collapse.
    
    These calls create all possible node heads for the cgroup at once, so the
    migration code (between nodes) doesn't need any special care.
    
    Link: https://lore.kernel.org/20260527204757.2544958-10-hannes@cmpxchg.org
    Signed-off-by: Johannes Weiner <hannes@cmpxchg.org>
    Reported-by: Mikhail Zaslonko <zaslonko@linux.ibm.com>
    Tested-by: Mikhail Zaslonko <zaslonko@linux.ibm.com>
    Acked-by: Shakeel Butt <shakeel.butt@linux.dev>
    Reviewed-by: Lorenzo Stoakes (Oracle) <ljs@kernel.org>
    Acked-by: Usama Arif <usama.arif@linux.dev>
    Reviewed-by: Kairui Song <kasong@tencent.com>
    Cc: Baolin Wang <baolin.wang@linux.alibaba.com>
    Cc: Barry Song <baohua@kernel.org>
    Cc: Dave Chinner <david@fromorbit.com>
    Cc: David Hildenbrand (Arm) <david@kernel.org>
    Cc: Dev Jain <dev.jain@arm.com>
    Cc: Lance Yang <lance.yang@linux.dev>
    Cc: Liam R. Howlett <liam@infradead.org>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Muchun Song <muchun.song@linux.dev>
    Cc: Nico Pache <npache@redhat.com>
    Cc: Roman Gushchin <roman.gushchin@linux.dev>
    Cc: Ryan Roberts <ryan.roberts@arm.com>
    Cc: Vasily Gorbik <gor@linux.ibm.com>
    Cc: Vlastimil Babka <vbabka@kernel.org>
    Cc: Zi Yan <ziy@nvidia.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- include/linux/huge_mm.h
+++ include/linux/huge_mm.h
@@ -439,10 +439,10 @@ static inline int split_huge_page(struct page *page)
 {
 	return split_huge_page_to_list_to_order(page, NULL, 0);
 }
+
+int folio_memcg_alloc_deferred(struct folio *folio);
+
 void deferred_split_folio(struct folio *folio, bool partially_mapped);
-#ifdef CONFIG_MEMCG
-void reparent_deferred_split_queue(struct mem_cgroup *memcg);
-#endif
 
 void __split_huge_pmd(struct vm_area_struct *vma, pmd_t *pmd,
 		unsigned long address, bool freeze);
@@ -680,7 +680,6 @@ static inline int try_folio_split_to_order(struct folio *folio,
 }
 
 static inline void deferred_split_folio(struct folio *folio, bool partially_mapped) {}
-static inline void reparent_deferred_split_queue(struct mem_cgroup *memcg) {}
 #define split_huge_pmd(__vma, __pmd, __address)	\
 	do { } while (0)
 
